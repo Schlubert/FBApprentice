1f717af12d3ca3c76c58b884a52656c035bb743c
Kevin <schlubert@gmail.com>
2025-11-17 14:47:26 +1300

    bulk changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/FBApprentice/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
-
